0775ff524bf8b7d64ed69dd523f160cf49f3f6c9
ouoertheo <jamin@tomeofjamin.net>
2023-12-17 14:03:31 -0600

    yet another attempt at getting go binary

--- Dockerfile.go
+++ Dockerfile.go
@@ -6,11 +6,11 @@ FROM golang:1.19-alpine
 WORKDIR /app/api
 
 # Copy the built Go binary from the first stage
-COPY  HACApi /app/api/HACApi
 COPY .env /app/api/.env
+RUN go install github.com/Threqt1/HACApi@latest
 
 # Expose any necessary ports
 EXPOSE 3000
 
 # Define the command to run when the container starts
-CMD ["sudo /app/api/HACApi"]
+CMD ["sudo HACApi"]
