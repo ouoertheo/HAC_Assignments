862503be38c540c32399b380ba0680a4d236ee63
ouoertheo <jamin@tomeofjamin.net>
2023-12-17 13:50:59 -0600

    no go build

--- Dockerfile.go
+++ Dockerfile.go
@@ -1,34 +1,16 @@
 # Dockerfile.go
 
-# Stage 1: Build the Go application
-FROM golang:1.19 as go-builder
-
-# Set the working directory inside the container
-WORKDIR /go/app
-
-# Install git to clone the repository
-RUN apt install git
-
-# Clone the Go application source code into the container
-ARG REPO_URL=https://github.com/Threqt1/HACApi.git
-RUN git clone $REPO_URL .
-COPY .env .env
-
-# Build the Go application
-RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o /go/bin/go-app
-
-# Stage 2: Create the final image
-FROM alpine:latest
-
-# Copy the built Go binary from the first stage
-COPY --from=go-builder /go/bin/go-app /app/api/go-app
-COPY --from=go-builder /go/app/.env /app/api/.env
+FROM golang:1.19-alpine
 
 # Set the working directory inside the container
 WORKDIR /app/api
+
+# Copy the built Go binary from the first stage
+COPY  HACApi /app/api/HACApi
+COPY .env /app/api/.env
 
 # Expose any necessary ports
 EXPOSE 3000
 
 # Define the command to run when the container starts
-CMD ["/app/api/go-app"]
+CMD ["/app/api/HACApi"]
